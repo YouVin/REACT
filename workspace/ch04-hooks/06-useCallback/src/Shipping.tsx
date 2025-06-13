3f56acfc5f43d9e622b2c6feadc324f050f74bb9
YouVin <dbqls990909@naver.com>
2025-06-13 13:31:58 +0900

    06 useCallback(함수 자체를 memoize), React.memo(컴포넌트를 memoize)

--- workspace/ch04-hooks/06-useCallback/src/Shipping.tsx
+++ workspace/ch04-hooks/06-useCallback/src/Shipping.tsx
@@ -1,15 +1,22 @@
 interface ShippingProps {
-  fees: number;
+  shippingFees: number;
   handlePayment: () => void;
 }
 
 // TODO 컴포넌트를 메모이제이션 해서 불필요한 리렌더링 방지
-function Shipping() {
-  console.log('Shipping 렌더링.');
-  
+function Shipping({ shippingFees, handlePayment }: ShippingProps) {
+  console.log("Shipping 렌더링.");
+
   return (
     <>
-      
+      <h2>배송비</h2>
+      <div>
+        배송비: {shippingFees.toLocaleString()}원<br />
+      </div>
+      <br />
+      <button type="button" onChange={handlePayment}>
+        결제
+      </button>
     </>
   );
 }
