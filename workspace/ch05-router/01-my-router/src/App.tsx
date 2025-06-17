cc448f9cc0253ef85520a672d0aa39dae9d7abce
YouVin <dbqls990909@naver.com>
2025-06-18 01:05:17 +0900

    React-router-dom

--- workspace/ch05-router/01-my-router/src/App.tsx
+++ workspace/ch05-router/01-my-router/src/App.tsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
 import Home from "@pages/Home";
+import Page1 from "@pages/Page1";
+import Page2 from "@pages/Page2";
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );  
+  const [currentPath, setCurrentPath] = useState(location.pathname);
+
+  useEffect(() => {
+    const handleLocationChange = () => {
+      console.log(location.pathname, "으로 주소 변경됨");
+      setCurrentPath(location.pathname);
+    };
+
+    window.addEventListener("popstate", handleLocationChange);
+
+    return () => {
+      window.removeEventListener("popstate", handleLocationChange); // ✅ 정리!
+    };
+  }, []);
+
+  const renderComponent = () => {
+    switch (currentPath) {
+      case "/":
+      case "/home":
+        return <Home />;
+      case "/page1":
+        return <Page1 />;
+      case "/page2":
+        return <Page2 />;
+      default:
+        return <p>404에러</p>;
+    }
+  };
+
+  return <>{renderComponent()}</>;
 }
 
-export default App
+export default App;
