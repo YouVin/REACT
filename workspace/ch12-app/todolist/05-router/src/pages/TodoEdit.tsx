32f701b22575bd3c4e8cbb6c77bce65840600597
YouVin <dbqls990909@naver.com>
2025-06-19 10:34:44 +0900

    api 서버에 수정 요청

--- workspace/ch12-app/todolist/05-router/src/pages/TodoEdit.tsx
+++ workspace/ch12-app/todolist/05-router/src/pages/TodoEdit.tsx
@@ -1,5 +1,6 @@
 import type { TodoItem } from "@pages/TodoInfo";
 import { Link, useNavigate, useOutletContext } from "react-router";
+import { useForm } from "react-hook-form";
 
 interface OutletContextProps {
   item: TodoItem;
@@ -12,9 +13,20 @@ function TodoEdit() {
   // const { item }: OutletContextProps = useOutletContext();
   const { item }: OutletContextProps = useOutletContext();
 
-  const updateTodo = (event: React.FormEvent) => {
-    event.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TodoItem>({
+    defaultValues: {
+      title: item.title,
+      content: item.content,
+      done: item.done,
+    },
+  });
 
+  const updateTodo = (formData: TodoItem) => {
+    console.log("api 서버에 수정 요청", formData);
     alert("할일이 수정 되었습니다");
 
     // 상세보기로 이동
@@ -24,9 +36,19 @@ function TodoEdit() {
     <div id="main">
       <h2>할일 수정</h2>
       <div className="todo">
-        <form onSubmit={updateTodo}>
+        <form onSubmit={handleSubmit(updateTodo)}>
           <label htmlFor="title">제목 :</label>
-          <input type="text" id="title" defaultValue={item.title} autoFocus />
+          <input
+            type="text"
+            id="title"
+            {...register("title", {
+              required: "제목을 입력하세요.",
+              pattern: {
+                value: /\s/,
+                message: "제목에 공백만 입력할 수 없습니다.",
+              },
+            })}
+          />
           <br />
           <label htmlFor="content">내용 :</label>
           <textarea
@@ -35,7 +57,6 @@ function TodoEdit() {
             rows={5}
             defaultValue={item.content}
           />
-
           <br />
           <label htmlFor="done">완료 :</label>
           <input type="checkbox" id="done" defaultChecked={item.done} />
