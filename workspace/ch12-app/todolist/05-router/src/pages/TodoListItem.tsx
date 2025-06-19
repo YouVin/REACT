2e0cc385ddecec7e8aef6c370e2d3666a7547c83
YouVin <dbqls990909@naver.com>
2025-06-19 10:34:25 +0900

    handleDelete 연결

--- workspace/ch12-app/todolist/05-router/src/pages/TodoListItem.tsx
+++ workspace/ch12-app/todolist/05-router/src/pages/TodoListItem.tsx
@@ -3,14 +3,19 @@ import { Link } from "react-router";
 
 interface TodoListProps {
   item: TodoItem;
+  handleDelete: (_id: number) => void;
 }
 
-function TodoListItem({ item }: TodoListProps) {
+function TodoListItem({ item, handleDelete }: TodoListProps) {
   return (
     <li>
       <span>{item._id}</span>
-      <Link to={`/list/${item._id}`}>{item.title}</Link>
-      <Link to="/list">삭제</Link>
+      <Link to={`/list/${item._id}`}>
+        {item.done ? <s>{item.title}</s> : item.title}
+      </Link>
+      <button type="button" onClick={() => handleDelete(item._id)}>
+        삭제
+      </button>
     </li>
   );
 }
