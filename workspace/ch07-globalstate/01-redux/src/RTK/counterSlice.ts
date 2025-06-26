4c67a96bcbed90d3c658af58e08e8fe6738d560a
YouVin <dbqls990909@naver.com>
2025-06-26 14:03:18 +0900

    RTK

--- workspace/ch07-globalstate/01-redux/src/RTK/counterSlice.ts
+++ workspace/ch07-globalstate/01-redux/src/RTK/counterSlice.ts
@@ -6,7 +6,7 @@ import { createSlice } from "@reduxjs/toolkit";
 // actions: 각 리듀서에 해당하는 액션 생성자 객체
 // caseReducers: 슬라이스 내부에서 정의된 리듀서 객체
 const counterSlice = createSlice({
-  name: "myCounter", // 슬라이스 이름(액션 타입의 접두사로 사용됨)
+  name: 'myCounter', // 슬라이스 이름(액션 타입의 접두사로 사용됨)
   initialState: { count: 10 }, // 초기 상태
   reducers: {
     countUp: (state, action) => {
@@ -16,10 +16,10 @@ const counterSlice = createSlice({
     countDown: (state, action) => {
       state.count -= action.payload;
     },
-    countReset: (state) => {
+    countReset: state => {
       state.count = 0;
-    },
-  },
+    }
+  }
 });
 
 console.log(counterSlice);
--- workspace/ch07-globalstate/01-redux/src/redux/counterActionCreactor.ts
+++ workspace/ch07-globalstate/01-redux/src/redux/counterActionCreactor.ts
@@ -6,7 +6,7 @@ export const COUNTER_ACTION = {
 
 // Store에 전달할 액션을 생성해서 반환
 // Store에서 다시 Reducer로 전달
-// { type: 'countUp', payload: { step: 2 } }
+// action 예시 { type: 'countUp', payload: { step: 2 } }
 const counterActionCreator = {
   countUp: (step: number) => ({ type: COUNTER_ACTION.UP, payload: { step } }),
   countDown: (step: number) => ({
